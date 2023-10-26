04c77f42586fbbb9d24ca91a1a95c391070f646f
Benito Palacios Sanchez <pleonex@protonmail.com>
2023-10-26 19:47:12 +0200

    :sparkles: Implement frames buffer

---
+++ src/PlayMobic.Tests/Video/FramesBufferTest.cs
@@ -0,0 +1,49 @@
+﻿namespace PlayMobic.Tests.Video;
+
+using PlayMobic.Video;
+
+[TestFixture]
+internal class FramesBufferTest
+{
+    [Test]
+    public void CurrentIsFirst()
+    {
+        var buffer = new FramesBuffer<FrameYuv420>(6, () => new FrameYuv420(256, 192));
+
+        Assert.That(buffer.Current, Is.SameAs(buffer.Buffer[0]));
+    }
+
+    [Test]
+    public void RotateShiftOne()
+    {
+        var buffer = new FramesBuffer<FrameYuv420>(3, () => new FrameYuv420(256, 192));
+        var frame0 = buffer.Buffer[0];
+        var frame1 = buffer.Buffer[1];
+
+        buffer.Rotate();
+
+        Assert.Multiple(() => {
+            Assert.That(buffer.Buffer.Count, Is.EqualTo(3));
+            Assert.That(buffer.Buffer[1], Is.SameAs(frame0));
+            Assert.That(buffer.Buffer[2], Is.SameAs(frame1));
+        });
+    }
+
+    [Test]
+    public void RotateReuseLastAsCurrent()
+    {
+        var buffer = new FramesBuffer<FrameYuv420>(3, () => new FrameYuv420(256, 192));
+        var current = buffer.Current;
+        var last = buffer.Buffer[^1];
+
+        buffer.Rotate();
+
+        Assert.Multiple(() => {
+            Assert.That(buffer.Buffer.Count, Is.EqualTo(3));
+            Assert.That(buffer.Current, Is.SameAs(last));
+            Assert.That(buffer.Buffer[0], Is.SameAs(last));
+
+            Assert.That(buffer.Buffer[1], Is.SameAs(current));
+        });
+    }
+}
---
+++ src/PlayMobic/Video/FramesBuffer.cs
@@ -0,0 +1,35 @@
+﻿namespace PlayMobic.Video;
+
+using System;
+
+internal class FramesBuffer<T>
+{
+    private readonly T[] buffer;
+
+    public FramesBuffer(int count, Func<T> initFactory)
+    {
+        buffer = new T[count];
+        for (int i = 0; i < count; i++) {
+            buffer[i] = initFactory();
+        }
+    }
+
+    public IReadOnlyList<T> Buffer => buffer;
+
+    public T Current => buffer[0];
+
+    public void Rotate()
+    {
+        // We put the current frame at the beginning of the buffer (pos 1), and push others one position.
+        // We discard the last frame in the buffer
+        T discarded = buffer[^1];
+
+        // Important: start backwards or we will placing the same item over and over.
+        for (int i = buffer.Length - 1; i > 0; i--) {
+            buffer[i] = buffer[i - 1];
+        }
+
+        // Trick to avoid allocating more memory: the discarded frame is our new frame to fill
+        buffer[0] = discarded;
+    }
+}
