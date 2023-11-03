6d064034b1cdcec3dcb83837758ab6b889176651
Benito Palacios Sanchez <pleonex@protonmail.com>
2023-11-03 06:31:17 +0100

    ✨ Improve DCT and quantization

--- src/PlayMobic/Video/Mobiclip/DiscreteCosineTransformer.cs
+++ src/PlayMobic/Video/Mobiclip/DiscreteCosineTransformer.cs
@@ -3,28 +3,36 @@
 /// <summary>
 /// Approximated integer based DCT transformation.
 /// </summary>
+/// <remarks>
+/// This is the same implementation as H.264.
+/// </remarks>
 internal class DiscreteCosineTransformer
 {
     public int[] InverseTransformation(int[] matrix, int size)
     {
         int[] output = new int[matrix.Length];
 
+        // hard-coded DC
         matrix[0] += 32;
 
+        // multiply matrix * IDCT
         for (int y = 0; y < size; y++) {
             InverseRow(matrix.AsSpan(y * size, size));
         }
 
         for (int y = 0; y < size; y++) {
+            // transpose matrix
             for (int x = y + 1; x < size; x++) {
                 int idx1 = (y * size) + x;
                 int idx2 = (x * size) + y;
                 (matrix[idx1], matrix[idx2]) = (matrix[idx2], matrix[idx1]);
             }
 
+            // multiply by IDCT transpose
             InverseRow(matrix.AsSpan(y * size, size));
 
             for (int x = 0; x < size; x++) {
+                // copy and de-scale factor 2^6 (scaled quantization)
                 output[(y * size) + x] = matrix[(y * size) + x] >> 6;
             }
         }
@@ -35,12 +43,12 @@ internal class DiscreteCosineTransformer
     private static void InverseRow(Span<int> row)
     {
         if (row.Length == 4) {
-            Inverse4(row);
+            InverseRow4(row);
             return;
         }
 
         Span<int> tmp = stackalloc int[4] { row[0], row[2], row[4], row[6] };
-        Inverse4(tmp);
+        InverseRow4(tmp);
 
         int e = row[7] + row[1] - row[3] - (row[3] >> 1);
         int f = row[7] - row[1] + row[5] + (row[5] >> 1);
@@ -61,8 +69,9 @@ internal class DiscreteCosineTransformer
         row[7] = tmp[0] - x0;
     }
 
-    private static void Inverse4(Span<int> row)
+    private static void InverseRow4(Span<int> row)
     {
+        // multiply matrix row by IDCT 4x4
         int a = row[0] + row[2];
         int b = row[0] - row[2];
         int c = row[1] + (row[3] >> 1);
